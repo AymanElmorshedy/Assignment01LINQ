158ec315adc1fd926320b96f972016a81a959605
ayman <aymanehab2002@gmail.com>
2025-02-09 17:15:14 +0200

    LINQ-Restriction - Operator Q03

--- Assignment01LINQ/Program.cs
+++ Assignment01LINQ/Program.cs
@@ -13,11 +13,21 @@ namespace Assignment01LINQ
             #endregion
 
             #region Q02
-            var result = ProductsList.Where(p => p.UnitsInStock > 0 && p.UnitPrice > 3);
-            foreach (var item in result)
-                Console.WriteLine(item);
+            //var result = ProductsList.Where(p => p.UnitsInStock > 0 && p.UnitPrice > 3);
+            //foreach (var item in result)
+            //    Console.WriteLine(item);
             #endregion
 
+            #region Q03
+            string[] arr = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+            var result = arr
+                .Select((word, index) => new { word, index })
+                .Where(x => x.word.Length < x.index)
+                .Select(x => x.index);
+
+            Console.WriteLine("Digits whose name is shorter than their value: " + string.Join(", ", result));
+            #endregion
             #endregion
         }
     }
